4913b6c950bb637a7bd2ed9cd2805692ebb06ea4
kimeumana <109218085+kimeumana@users.noreply.github.com>
2025-07-22 16:16:10 +0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,15 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#221111] text-white">
+      <Header />
+      <main className="p-10">
+        <h1 className="text-3xl font-bold">Welcome to Ma3 App 👋</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
 
