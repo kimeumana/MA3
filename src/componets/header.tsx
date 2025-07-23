f94454ccc211aab39bcf5e5227dce944a6954ae3
kimeumana <109218085+kimeumana@users.noreply.github.com>
2025-07-23 16:43:09 +0300

    Update header.tsx

--- src/componets/header.tsx
+++ src/componets/header.tsx
@@ -1,6 +1,7 @@
 const Header = () => {
   return (
-    <header className="bg-[#2c1212] px-6 py-4 border-b border-[#472424] flex justify-between items-center">
+    <header className="bg-dark text-primary shadow-xl rounded-xl">
+
       <h1 className="text-xl font-semibold">Ma3 App</h1>
       <div className="text-sm text-gray-300">Built with Tailwind + React</div>
     </header>
