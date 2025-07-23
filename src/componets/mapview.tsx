5f4cdf668f6abb8c7d992fed75eeeb5ac3255a8d
kimeumana <109218085+kimeumana@users.noreply.github.com>
2025-07-23 16:47:29 +0300

    Update mapview.tsx

--- src/componets/mapview.tsx
+++ src/componets/mapview.tsx
@@ -1,1 +1,20 @@
+import { useEffect } from "react"
+
+const MapView = () => {
+  useEffect(() => {
+    // @ts-ignore
+    const loader = new window.google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center: { lat: -1.286389, lng: 36.817223 },
+      zoom: 12,
+    })
+  }, [])
+
+  return (
+    <div className="w-full h-[500px] rounded-md border border-gray-700" id="map">
+      {/* Google Map loads here */}
+    </div>
+  )
+}
+
+export default MapView
 
