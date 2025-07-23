d82c90c27484d0d1412a0d7dc7ea9d2472659161
kimeumana <109218085+kimeumana@users.noreply.github.com>
2025-07-23 16:47:56 +0300

    Update footer.tsx

--- src/componets/footer.tsx
+++ src/componets/footer.tsx
@@ -1,1 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#111] text-gray-400 text-sm py-4 text-center">
+      &copy; {new Date().getFullYear()} Ma3 App by Kimeu. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
 
